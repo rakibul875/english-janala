c7ab8c628bda2e8206874fb22d08454fdb232911
rakibul875 <rakibulislamashik78@gmail.com>
2026-03-04 13:38:48 +0600

    complit lessons button section and add java-script

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log("hello wold")
+const lodLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then(res => res.json())
+        .then(json => displayLesson(json.data))
+}
+
+const displayLesson = (lessons) =>{
+   const levelContainer=document.getElementById("level-container")
+   levelContainer.innerHTML=""
+
+   for(let lesson of lessons){
+    const buttonDive=document.createElement("div")
+
+    buttonDive.innerHTML=`
+    <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Learn - ${lesson.level_no}</button>
+    `
+    levelContainer.append(buttonDive)
+   }
+}
+
+    lodLessons()
